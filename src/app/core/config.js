7a29d88a183f856acf662a58b8d2718e4850af2e
Luiz Augusto <luiz66@gmail.com>
2015-01-12 14:20:39 -0300

    Removing my access token

--- src/app/core/config.js
+++ src/app/core/config.js
@@ -8,7 +8,7 @@
     config.$inject = ['$httpProvider'];
     /* @ngInject */
     function config($httpProvider) {
-    	var accessToken = '1ff19f35ddd2d90a8e66b4e36221f4488a489acddf26a4ce75136bcf8484cafb';
+    	var accessToken = 'client-access-token';
         $httpProvider.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
     }
 })();
